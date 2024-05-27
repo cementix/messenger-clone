eb080797a81bdf21bd8f0dbd9b951a97c732078f
cementix <bodakondrackij@gmail.com>
2024-05-27 21:02:30 +0300

    auth ui

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
-        {/* Auth form */}
+        <AuthForm />
       </div>
     </main>
   );
