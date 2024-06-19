946c7e4c364f31b08a9182ee5a3559a89d2dd709
cementix <bodakondrackij@gmail.com>
2024-06-20 02:17:18 +0300

    post function

---
+++ app/api/messanges/route.ts
@@ -0,0 +1,68 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await req.json();
+    const { message, image, conversationId } = body;
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId,
+          },
+        },
+        sender: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+        seen: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+    });
+
+    const updatedConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage, { status: 200 });
+  } catch (error) {
+    console.log(error, "ERROR_MESSAGES");
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
