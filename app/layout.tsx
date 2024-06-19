7974ed5694af8aeae3fd0a8339088fc1471cec79
cementix <bodakondrackij@gmail.com>
2024-06-20 02:19:29 +0300

    route fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Messanger Clone",
+  title: "Messenger Clone",
   description: "Next.js Messanger Clone",
 };
 
